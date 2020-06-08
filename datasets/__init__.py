9da7cdfe8209da7aaa0e7d070d29a4e168104dbe
hong6us <41441659+hong6us@users.noreply.github.com>
2020-06-08 12:18:15 -0600

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,2 +1,3 @@
 from .voc0712 import VOCDetection, VOC_CLASSES
 from .augmentation import get_augumentation, detection_collate, Resizer, Normalizer, Augmenter, collater
+from .handipark import HandiParkDetection, HandiPark_CLASSES
