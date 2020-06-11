baa3627fc30aac78d61527eab47dd220dd17ae38
hong6us <41441659+hong6us@users.noreply.github.com>
2020-06-11 10:51:36 -0600

    Update augmentation.py

--- datasets/augmentation.py
+++ datasets/augmentation.py
@@ -110,7 +110,7 @@ class Resizer(object):
 
         new_image = np.zeros((common_size, common_size, 3))
         new_image[0:resized_height, 0:resized_width] = image
-        annots[:, :4] *= scale
+        annots[:, :2] *= scale
 
         return {'img': torch.from_numpy(new_image), 'annot': torch.from_numpy(annots), 'scale': scale}
 
